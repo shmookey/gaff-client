0436075551ca327092b1c77782c85d26c195007b
Luke Williams <shmookey@shmookey.net>
2014-06-14 17:47:17 +0000

    Action-based tooltips.

--- js/scene-interaction.js
+++ js/scene-interaction.js
@@ -1,5 +1,6 @@
 game.service ('SceneInteraction', ['$timeout', 'Player', 'Character', 'Dialogue', 'ItemInspector', 'Scene',
   function($timeout, Player, Character, Dialogue, ItemInspector, Scene) {
+    var self = this;
     function RunAction (action) {
         for (var i=0; i<action.length; i++) {
             var command = action[i];
@@ -51,27 +52,35 @@ game.service ('SceneInteraction', ['$timeout', 'Player', 'Character', 'Dialogue'
         }
 
         // Run the action for the first mapping condition that passes
-        for (var i=0; i<actionMap.length; i++) {
-            var mapping = actionMap[i];
-            if (mapping.condition && !Player.evaluateCondition (mapping.condition)) continue;
-            var actionParts = mapping.action.split ('::');
-            
-            if (actionParts[0] == 'Talk') {
-                var character = Character.get(interaction.linkedCharacter);
-                var dialogueName = actionParts[1];
-                var flag = character.name.replace(' ','_') + '::~Active';
-                Player.grantFlag (flag);
-                Dialogue.beginDialogue (character, dialogueName, interaction, function () {
-                    Player.revokeFlag (flag);
-                });
-            } else {
-                // Other action types are considered generic
-                var action = interaction.actions[mapping.action];
-                RunAction(action);
-            }
+        var mapping = self.determineAction (actionMap);
+        if (!mapping) {
+            console.log ('No passing action mappings found!');
             return;
         }
-        console.log ('No passing action mappings found!');
+
+        var actionParts = mapping.action.split ('::');        
+        if (actionParts[0] == 'Talk') {
+            var character = Character.get(interaction.linkedCharacter);
+            var dialogueName = actionParts[1];
+            var flag = character.name.replace(' ','_') + '::~Active';
+            Player.grantFlag (flag);
+            Dialogue.beginDialogue (character, dialogueName, interaction, function () {
+                Player.revokeFlag (flag);
+            });
+        } else {
+            // Other action types are considered generic
+            var action = interaction.actions[mapping.action];
+            RunAction(action);
+        }
+    };
+
+    this.determineAction = function (actionMap) {
+        /* Returns the first passing rule in the action map. */
+
+        for (var i=0; i<actionMap.length; i++) {
+            var mapping = actionMap[i];
+            if (!mapping.condition || Player.evaluateCondition (mapping.condition)) return mapping;
+        }
     };
 
     this.determineState = function (interaction) {
@@ -94,22 +103,16 @@ game.service ('SceneInteraction', ['$timeout', 'Player', 'Character', 'Dialogue'
 }]);
 
 game.directive ('sceneInteraction', function () {
-    var link = function (scope, element, attrs) {
-        $(element).tooltip();
-        scope.setTooltip = function (tooltip) {
-            $(element).attr('title',tooltip).tooltip('fixTitle');
-        };
-    };
-
     var controller = ['$scope', 'SceneInteraction', 'Assets', 'Player', 'Debugger', 
       function ($scope, SceneInteraction, Assets, Player, Debugger) {
         $scope.css = {
-            left: "0px",
-            top: "0px",
-            width: "0px",
-            height: "0px"
+            left: '0px',
+            top: '0px',
+            width: '0px',
+            height: '0px'
         };
         $scope.showRegion = false;
+        $scope.tooltip = '';
         
         // Attach all images in advance to avoid flicker when changing
         var imageLayers = []; // Map state name to layer index
@@ -142,11 +145,6 @@ game.directive ('sceneInteraction', function () {
             $scope.css['background-size'] = bgSize;
         }
 
-        //if ($scope.model.overlayImage) {
-        //    var imageURI = Assets.getImageURI ($scope.model.overlayImage);
-        //    $scope.css['background-image'] = 'url(' + imageURI + ')';
-        //}
-
         function ApplyState (state) {
             var region = state.region;
             //var imageURI = Assets.getImageURI (state.image);
@@ -157,15 +155,11 @@ game.directive ('sceneInteraction', function () {
                 $scope.css['width'] = region[2] + "px";
                 $scope.css['height'] = region[3] + "px";
             }
-            $scope.setTooltip (state.tooltip);
+            var tooltip = '';
+            var mapping = SceneInteraction.determineAction($scope.model.actionMappings[$scope.model.defaultAction]);
+            if (state.tooltip)
+                $scope.tooltip = mapping.verb + ' ' + state.tooltip;
         }
-
-        //$scope.$watch(function(){return $scope.model.region;}, function(region) {
-        //    $scope.css['left'] = region[0] + "px";
-        //    $scope.css['top'] = region[1] + "px";
-        //    $scope.css['width'] = region[2] + "px";
-        //    $scope.css['height'] = region[3] + "px";
-        //}, true);
 
         // Watch for changes in the progress serial, so that we can keep the
         // interaction state up to date.
@@ -186,8 +180,7 @@ game.directive ('sceneInteraction', function () {
     return {
         restrict: 'E',
         controller: controller,
-        link: link,
-        replace: true,
+        replace: false,
         templateUrl: 'partials/scene-interaction.html',
         scope: {
             model: '=ngModel',
